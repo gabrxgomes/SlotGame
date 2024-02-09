16a50e68eb8f930c72bdfa3bfc88ab3303202a05
Gabriel Gomes <100102446+gabrxgomes@users.noreply.github.com>
2024-02-09 12:29:53 -0300

    Deixando fixo o meu balance
    Adicionando um método para deixar fixo o meu balance mesmo que eu perca após fazer uma aposta.

--- SlotGame/Form1.cs
+++ SlotGame/Form1.cs
@@ -22,11 +22,28 @@ namespace SlotGame
         }
 
         Random rnd = new Random();
-        int a, b, c, move, wins, balance, loss;
+        int a, b, c, move, wins, balance, loss, tempbalance;
 
         void Game_Result()
         {
-            if (System.Convert.ToInt32(a & b) != c)
+            //if (System.Convert.ToInt32(a & b ) == c )
+            //if (System.Convert.ToInt32(a & b) != c)
+            if (System.Convert.ToInt32(a == b) != c)
+                //if (System.Convert.ToInt32(a == b & a == c & b == c) == c)
+                {
+                wins++;
+                lbl_win.Text = "Wins :" + wins;
+                balance += 2;
+                lbl_balance.Text = "Balance : $" + Convert.ToInt32(textBox1.Text) * balance;
+                btn_play.Text = "Continue Playing...";
+                lbl_cassino.Text = "You Won !!!..";
+
+
+
+            }
+
+
+            else if (System.Convert.ToInt32(b == c) != a)//1
             {
                 wins++;
                 lbl_win.Text = "Wins :" + wins;
@@ -35,19 +52,81 @@ namespace SlotGame
                 btn_play.Text = "Continue Playing...";
                 lbl_cassino.Text = "You Won !!!..";
             }
+
+            else if (System.Convert.ToInt32(c == a) != b)//2
+            {
+                wins++;
+                lbl_win.Text = "Wins :" + wins;
+                balance += 2;
+                lbl_balance.Text = "Balance : $" + Convert.ToInt32(textBox1.Text) * balance;
+                btn_play.Text = "Continue Playing...";
+                lbl_cassino.Text = "You Won !!!..";
+            }
+            
+            /*if (balance >= 50) //tatica de roubar no jogo, definimos um teto para um balance saudavel mas ainda
+                                //de forma mais burra, vamos ir estudando.
+            {
+                wins -= 1;
+                pictureBox1.Image = Properties.Resources.dollar;
+                pictureBox2.Image = Properties.Resources.dollar;
+                pictureBox3.Image = Properties.Resources.dollar;
+                lbl_cassino.Text = "Try Again";
+                balance = 0;
+                
+            }*/
+
+                    //ou
+
+                    /*if (wins >= 5)
+                    {
+                        pictureBox1.Image = Properties.Resources.dollar;
+                        pictureBox2.Image = Properties.Resources.dollar;
+                        pictureBox3.Image = Properties.Resources.dollar;
+                        lbl_cassino.Text = "Try Again";
+
+                    }*/
             else
             {
+                //wins--;
                 lbl_cassino.Text = "Try Again";
                 loss++;
                 lbl_loss.Text = "Loss : "+loss;
-                lbl_balance.Text = "Balance : $ 0";
-                balance = 0;
+
+                tempbalance = Convert.ToInt32(textBox1.Text) * balance - Convert.ToInt32(textBox1.Text);
+
+                MessageBox.Show("Conta" + tempbalance);
+
+                //lbl_balance.Text = "Balance : $" + Convert.ToInt32(textBox1.Text) * balance ;
+
+
+                //balance -= 2;
+
+
+                //lbl_balance.Text = "Balance : $ 0";
+                //lbl_balance.Text = "Balance : $ 0";
+
+                //tempbalance = balance - Convert.ToInt32(textBox1.Text);
+
+                //tempbalance -= balance;
+
+
+
+                lbl_balance.Text = "Balance : $" + tempbalance;
+
+                //MessageBox.Show("Conta" + tempbalance); //saida de teste como messagebox
+
+                //MessageBox.Show("Saiu Gold"); //saida de teste como messagebox
+
+                //lbl_balance.Text = "Balance : $" + (Convert.ToInt32(textBox1.Text) - balance);
+
+
                 textBox1.Text = "";
                 btn_play.Enabled = false;
                 pictureBox1.Image = Properties.Resources.dollar;
                 pictureBox2.Image = Properties.Resources.dollar;
                 pictureBox3.Image = Properties.Resources.dollar;
             }
+
         }
         private void btn_play_Click_1(object sender, EventArgs e)
         {
@@ -56,6 +135,10 @@ namespace SlotGame
                 MessageBox.Show("Please enter the Bid Amount inside the textbox to play.. ");
 
             }
+
+
+
+            
             else
             {
                 timer1.Enabled = true;
@@ -70,14 +153,18 @@ namespace SlotGame
         private void btn_bid_Click(object sender, EventArgs e)
         {
             lbl_cassino.Text = "Casino";
-            balance = 0;
+            //balance = 0;
+            
             textBox1.Enabled = true;
             textBox1.Text = "";
             textBox1.BackColor = Color.White;
             textBox1.Focus();
             btn_play.Enabled = true;
             btn_play.Text = "Play";
-            lbl_balance.Text = "Balance : $";
+
+            //lbl_balance.Text = "Balance : $" + balance;
+
+            //lbl_balance.Text = "Balance : $" + (Convert.ToInt32(textBox1.Text) - balance);
 
 
         }
@@ -137,9 +224,12 @@ namespace SlotGame
                         pictureBox3.Image = Properties.Resources.Bronze;
                         //MessageBox.Show("Saiu Bronze"); //saida de teste como messagebox
                         break;
+
                 }
+                
 
             }
+           
             else
             {
                 timer1.Enabled = false;
