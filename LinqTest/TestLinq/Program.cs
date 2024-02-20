208a409bf228f4f1632f1d78e9d7eac1789c5cbc
Gabriel Gomes <100102446+gabrxgomes@users.noreply.github.com>
2024-02-20 09:20:55 -0300

    Estudos com Linq
    Aprofundando estudos de c#, no momento revendo sobre linq.

---
+++ LinqTest/TestLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numeros = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var numerosFiltrados = numeros.Where(n => n > 5);
+
+        foreach (var numero in numerosFiltrados)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
